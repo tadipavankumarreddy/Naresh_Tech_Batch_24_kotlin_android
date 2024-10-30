277d40d94fde592c81215bbeee0d163ca44b1ab3
Pavan <tadipavankumarreddy@gmail.com>
2024-10-30 08:28:30 +0530

    New data

---
+++ RegisterMeJUC/app/src/main/java/com/nareshtech/registermejuc/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.nareshtech.registermejuc
+
+import android.content.Context
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nareshtech.registermejuc.ui.theme.RegisterMeJUCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RegisterMeJUCTheme {
+                registrationScreen()
+            }
+        }
+    }
+
+    @Composable
+    @Preview
+    private fun registrationScreen() {
+        var name by remember { mutableStateOf("") }
+        var age by remember { mutableStateOf("") }
+        var gender by remember { mutableStateOf("") }
+        var languages = listOf("Telugu", "Hindi", "Marati")
+        val selectedLanguages = remember { mutableStateListOf<String>() }
+
+        Column(modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(WindowInsets.statusBars.asPaddingValues())
+            .padding(WindowInsets.navigationBars.asPaddingValues())) {
+
+            // name
+            OutlinedTextField(value = name,
+                onValueChange = {name = it},
+                label = { Text(text = "Enter your name")},
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp))
+
+            // age
+            OutlinedTextField(value = age,
+                onValueChange = {age = it},
+                label = { Text(text = "Enter your age")},
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp))
+
+            // gender
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround ) {
+                Text(text = "Gender")
+
+                RadioButton(selected = gender == "Male", onClick = { gender = "Male" })
+                Text(text = "Male")
+
+                RadioButton(selected = gender == "Female", onClick = { gender = "Female" })
+                Text(text = "Female")
+            }
+
+            Text(text = "Languages", fontWeight = FontWeight.Bold)
+
+            Row(modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround) {
+                languages.forEach{language ->
+                    Checkbox(checked = selectedLanguages.contains(language),
+                        onCheckedChange = {
+                            if (it) selectedLanguages.add(language) else selectedLanguages.remove(language)
+                        })
+                    Text(text = language, modifier = Modifier.weight(1f))
+                }
+            }
+
+        }
+    }
+}
--- ScoreTrackerJUC/app/src/main/java/com/nareshtech/scoretrackerjuc/MainActivity.kt
+++ ScoreTrackerJUC/app/src/main/java/com/nareshtech/scoretrackerjuc/MainActivity.kt
@@ -4,36 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.nareshtech.scoretrackerjuc.ui.theme.ScoreTrackerJUCTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,41 +40,58 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScoreTrackerJUCTheme {
-                generateScreen()
+                val countViewModel:CountViewModel = ViewModelProvider(this)
+                    .get(CountViewModel::class.java)
+                generateScreen(countViewModel)
             }
         }
     }
 
+    class CountViewModel:ViewModel(){
+        var count by mutableStateOf(0)
+
+        fun increment(){
+            count++
+        }
+
+        fun decrement(){
+            count--
+        }
+    }
+
     @Composable
-    @Preview
-    fun generateScreen(){
-        var count by remember { mutableStateOf(0) }
-        Column(modifier = Modifier
-            .fillMaxSize()
+    fun generateScreen(countViewModel: CountViewModel){
+        var count = countViewModel.count
+        val paddingModifier = Modifier
             .padding(WindowInsets.statusBars.asPaddingValues())
-            .padding(20.dp)) {
-            Button(onClick = { count++ },
+            .padding(WindowInsets.navigationBars.asPaddingValues())
+            .padding(20.dp)
+
+        val textStyle = TextStyle(fontSize = 100.sp, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+
+        Column(modifier = Modifier
+            .fillMaxSize().then(paddingModifier)) {
+            Button(onClick = { countViewModel.increment()},
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)) {
                 Text(text = "+", fontSize = 20.sp)
             }
 
-            Box(modifier = Modifier.weight(8f)
+            Box(modifier = Modifier
+                .weight(8f)
                 .background(Color.Yellow)
-                .padding(16.dp),
+                .border(BorderStroke(5.dp, Color.White)),
                 contentAlignment = Alignment.Center){
 
                 Text(text = count.toString(),
                     modifier = Modifier.fillMaxWidth(),
-                    fontSize = 100.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center
+                    style = textStyle
                 )
 
             }
 
-            Button(onClick = { count-- },
+            Button(onClick = { countViewModel.decrement() },
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)) {
